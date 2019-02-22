e1f5ff37efb970a0408a1d04eb7370332fd45d51
Ulzee An <ul1994@gmail.com>
2019-02-22 02:16:51 +0000

    working s-stops example

--- models/Kernel.py
+++ models/Kernel.py
@@ -16,63 +16,60 @@ class Kernel(nn.Module):
 
 		self.insize = insize
 		self.hsize = hsize
-		# self.hsize = hsize
 
 		def inst_msgop(has_h=True):
 			v_in = nn.Sequential(
 				nn.Linear(1, hsize)
 			)
 			if has_h:
-				h_in = nn.Sequential(
-					nn.Linear(hsize, hsize)
-				)
-			msg_out = nn.Sequential(
-				nn.Linear(hsize * (2 if has_h else 1), hsize),
-				# nn.ReLU(),
-				# nn.Linear(hsize, hsize),
-				nn.Sigmoid(),
-				# nn.ReLU(),
-			)
+				# h_in = nn.Sequential(
+				# 	nn.Linear(hsize, hsize)
+				# )
+				h_in = None
+			# msg_out = nn.Sequential(
+			# 	nn.Linear(hsize * (2 if has_h else 1), hsize),
+			# 	# nn.ReLU(),
+			# 	# nn.Linear(hsize, hsize),
+			# 	nn.Sigmoid(),
+			# 	# nn.ReLU(),
+			# )
+			msg_out = None
 			# return v_in, h_in, msg_out
-			if has_h:
-				return nn.ModuleList([v_in, h_in, msg_out])
-			else:
-				return nn.ModuleList([v_in, msg_out])
+			# if has_h:
+			# 	return nn.ModuleList([v_in, h_in, msg_out])
+			# else:
+				# return nn.ModuleList([v_in, msg_out])
+			return nn.ModuleList([v_in])
 
-		self.mops_0 = nn.ModuleList()
-		for _ in range(insize):
-			self.mops_0.append(inst_msgop(has_h=False))
+		# self.mops_0 = nn.ModuleList()
+		# for _ in range(insize):
+		# 	self.mops_0.append(inst_msgop(has_h=False))
 
 		self.mops = nn.ModuleList() # independent weights per neighbor
 		for _ in range(insize):
 			self.mops.append(inst_msgop(has_h=True))
-		# self.upop = nn.Sequential(
-		# 	nn.Linear(hsize * 2, hsize + 1)
-		# )
 
 	def forward(self, node, initial=False):
-		if len(node.ns) == 0:
-			node.msg = None
-			return None
+		assert len(node.ns)
 
 		msgs = []
-		rel_nodes = node.ns + [node] # incl. itself
-		# rel_nodes = node.ns # incl. itself
-		if initial:
-			for (v_in, msg_out), other in zip(self.mops_0, rel_nodes):
-				mix = v_in(other.v)
-				msgout = msg_out(mix)
-				msgs.append(msgout)
-		else:
-			for (v_in, h_in, msg_out), other in zip(self.mops, rel_nodes):
-				mix = torch.cat([v_in(other.v), h_in(other.h)], dim=1)
-				msgout = msg_out(mix)
-				msgs.append(msgout)
+		# rel_nodes = node.ns + [node] # incl. itself
+		rel_nodes = node.ns # incl. itself
+		# if initial:
+		for (v_in,), other in zip(self.mops, rel_nodes):
+			mix = v_in(other.v)
+			# msgout = msg_out(mix)
+			msgs.append(mix)
+		# else:
+		# 	for (v_in, h_in, msg_out), other in zip(self.mops, rel_nodes):
+		# 		mix = torch.cat([v_in(other.v), h_in(other.h)], dim=1)
+		# 		msgout = msg_out(mix)
+		# 		msgs.append(msgout)
 		assert len(msgs)
 		msgs = torch.stack(msgs, dim=1).to(self.device)
-		update = torch.sum(msgs, dim=1)
-		node.msg = update
-		return update
+		msg_result = torch.sum(msgs, dim=1)
+		node.msg = msg_result
+		return msg_result
 
 class Update(nn.Module):
 	# name = 'cast'
@@ -80,21 +77,21 @@ class Update(nn.Module):
 		super(Update, self).__init__()
 
 		self.hsize = hsize
-		self.h_in = nn.Sequential(
-			nn.Linear(hsize, hsize)
-		)
-		self.msg_in = nn.Sequential(
-			nn.Linear(hsize, hsize)
-		)
-		self.h_dense = nn.Sequential(
-			nn.Linear(hsize * 2, hsize),
-			nn.ReLU(),
-			nn.Linear(hsize, hsize),
-			nn.ReLU(),
-			nn.Linear(hsize, hsize),
-			# nn.Dropout(0.5)
-			# nn.ReLU()
-		)
+		# self.h_in = nn.Sequential(
+		# 	nn.Linear(hsize, hsize)
+		# )
+		# self.msg_in = nn.Sequential(
+		# 	nn.Linear(hsize, hsize)
+		# )
+		# self.h_dense = nn.Sequential(
+		# 	# nn.Linear(hsize * 2, hsize),
+		# 	# nn.ReLU(),
+		# 	nn.Linear(hsize, hsize),
+		# 	# nn.ReLU(),
+		# 	# nn.Linear(hsize, hsize),
+		# 	# nn.Dropout(0.5)
+		# 	# nn.ReLU()
+		# )
 		# self.h_after = nn.Sequential(
 		# 	# nn.Linear(hsize, hsize),
 		# 	# nn.ReLU(),
@@ -102,12 +99,12 @@ class Update(nn.Module):
 		# 	# nn.Sigmoid(),
 		# 	# nn.ReLU()
 		# )
-		self.h_out = nn.Sequential(
-			nn.Linear(hsize, hsize),
-			nn.ReLU(),
-			nn.Linear(hsize, hsize),
-			nn.Sigmoid(),
-		)
+		# self.h_out = nn.Sequential(
+		# 	nn.Linear(hsize, hsize),
+		# 	nn.ReLU(),
+		# 	nn.Linear(hsize, hsize),
+		# 	nn.Sigmoid(),
+		# )
 		self.v_out = nn.Sequential(
 			# nn.Linear(hsize, hsize),
 			# nn.ReLU(),
@@ -118,22 +115,25 @@ class Update(nn.Module):
 
 
 	def forward(self, node):
-		if type(node.msg) is type(None): # an end node
-			return
+		assert type(node.msg) is not type(None)
 
-		mix = torch.cat([self.h_in(node.h), self.msg_in(node.msg)], dim=1)
+		# mix = torch.cat([self.h_in(node.h), self.msg_in(node.msg)], dim=1)
 
-		mix = self.h_dense(mix)
+		# mix = self.h_dense(mix)
+		mix = node.msg
+		node.msg = None
+		# in: seqlen x batch x dims
 		mix, hidden = self.rnn(mix.unsqueeze(0), node.hidden)
 		node.hidden = hidden
 		mix = mix.squeeze(0)
 		# mix = self.h_after(mix)
 
 		# h and v are updated
-		node.v, node.h = self.v_out(mix), self.h_out(mix)
+		# node.v, node.h = self.v_out(mix), self.h_out(mix)
+		node.v = self.v_out(mix)
 		# node.v, node.h = torch.split(mix, [1, self.hsize], dim=1)
 		# node.h = nn.Sigmoid()(node.h)
-		return node.v, node.h
+		return node.v
 
 class Node:
 	def __init__(self, value, zero, device=None):
@@ -192,40 +192,45 @@ def inst_tree(struct, node, device=None):
 	return nodeObj, params
 
 def zip_op(t1, t2, op):
-    ls = [(t1, t2)]
-    while len(ls):
-        n1, n2 = ls[0]
-        ls = ls[1:]
-        op(n1, n2)
-        for c1, c2 in zip(n1['ns'], n2.ns):
-            ls.append((c1, c2))
+	it = 0
+	ls = [(t1, t2)]
+	while len(ls):
+		n1, n2 = ls[0]
+		# print('zip', len(n1['ns']), type(n1['op']), it)
+		it += 1
+		ls = ls[1:]
+		op(n1, n2)
+		# print('end')
+		for c1, c2 in zip(n1['ns'], n2.ns):
+			ls.append((c1, c2))
 
 def message(kernels, graph_t, initial=False):
-    op = lambda kern, node: kern['op'](node, initial) if initial else \
-        lambda kern, node: kern['op'](node)
-    zip_op(kernels, graph_t, op=op)
+	# op = lambda kern, node: kern['op'](node, initial) if initial else \
+	# 	lambda kern, node: kern['op'](node)
+	op = lambda kern, node: kern['op'](node)
+	zip_op(kernels, graph_t, op=op)
 
 def update(upops, graph_t):
-    zip_op(upops, graph_t, op=lambda up, node: up['op'](node))
+	zip_op(upops, graph_t, op=lambda up, node: up['op'](node))
 
 def count_rec(node, cf):
 	return 1 + sum([count_rec(cn, cf) for cn in cf(node)])
 
 def gather_predictions(_node, node):
-    # end nodes do not hold convolved results, so they are ignored
-    ls = [(_node._v, node.v)] if len(node.ns) else []
-    for _nb, nb in zip(_node.ns, node.ns):
-        ls += gather_predictions(_nb, nb)
-    return ls
+	# end nodes do not hold convolved results, so they are ignored
+	ls = [(_node._v, node.v)] if len(node.ns) else []
+	for _nb, nb in zip(_node.ns, node.ns):
+		ls += gather_predictions(_nb, nb)
+	return ls
 
 def reassign_v(states, graph_t):
-    ls = [(states, graph_t)]
-    while len(ls):
-        n1, n2 = ls[0]
-        ls = ls[1:]
-        n1.v = n2._v.clone()
-        for c1, c2 in zip(n1.ns, n2.ns):
-            ls.append((c1, c2))
+	ls = [(states, graph_t)]
+	while len(ls):
+		n1, n2 = ls[0]
+		ls = ls[1:]
+		n1.v = n2._v.clone()
+		for c1, c2 in zip(n1.ns, n2.ns):
+			ls.append((c1, c2))
 
 if __name__ == '__main__':
 	model = Kernel()
